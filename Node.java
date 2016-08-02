f23e6791fb593abed444804f8a5a3ec2874550f1
Tina Wu <tinawu98@gmail.com>
2016-08-01 20:55:21 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,3 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ProjectSahara;
+
+/**
+ *
+ * @author tinawu
+ */
 public class Node {
     private String name;
     private Edge[] adjacencies;
@@ -17,6 +29,9 @@ public class Node {
     public void setAdjacentEdges(Edge[] setThis){
         adjacencies = setThis;
 
+    }
+    public String toString() {
+        return name;
     }
 
 }
