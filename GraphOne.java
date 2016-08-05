1ade2b7c7e847992dd364c80db6dbf479a555d5f
mattquan <quan.matthew1999@gmail.com>
2016-08-04 20:22:59 -0400

    MAJOR ORGANIZATION
    Everyone take a look at this. this is more dynamic and organized. give
    me thots/comments

---
+++ GraphOne.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectSahara;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Matt
+ */
+public class GraphOne {
+    public static ArrayList<Node> build() {
+ //Creating the graph
+    return new ArrayList<>(
+            Arrays.asList(
+                                   
+                new Node("A", new Edge[]{
+                    new Edge(2, "O", 0),
+                    new Edge(2, "B", 1),
+                    new Edge(7, "D", 2),
+                    new Edge(12, "F", 3),}),
+                new Node("B", new Edge[]{
+                    new Edge(5, "O", 0),
+                    new Edge(2, "A", 1),
+                    new Edge(4, "D", 2),
+                    new Edge(1, "C", 3),
+                    new Edge(3, "E", 4),}),
+                new Node("C", new Edge[]{
+                    new Edge(4, "O", 0),
+                    new Edge(1, "B", 1),
+                    new Edge(4, "E", 2),}),
+                new Node("D", new Edge[]{
+                    new Edge(4, "B", 0),
+                    new Edge(7, "A", 1),
+                    new Edge(1, "E", 2),
+                    new Edge(5, "T", 3),}),
+                new Node("E", new Edge[]{
+                    new Edge(4, "C", 0),
+                    new Edge(3, "B", 1),
+                    new Edge(1, "D", 2),
+                    new Edge(7, "T", 3),}),
+                new Node("F", new Edge[]{
+                    new Edge(12, "A", 0),
+                    new Edge(3, "T", 1),}),
+                new Node("O", new Edge[]{
+                    new Edge(2, "A", 0),
+                    new Edge(5, "B", 1),
+                    new Edge(4, "C", 2)}), 
+                new Node("T", new Edge[]{
+                    new Edge(3, "F", 0),
+                    new Edge(5, "D", 1),
+                    new Edge(7, "E", 2),})
+            ));
+    
+    }
+}
--- Main.java
+++ Main.java
@@ -10,83 +10,29 @@ import java.util.*;
  *
  * @author tinawu
  */
-public class Main {
+public class Main {   
     
-    static ArrayList<String> unselected ;
-    static ArrayList<Node> listOfNodes;
-    static ArrayList<Path> container;
+    
     
     public static void main(String[] args) {
-        listOfNodes = new ArrayList<Node>();
-        unselected = new ArrayList<String>();
-        //Creating the graph
-        Node O = new Node("O", new Edge[] {
-            new Edge(2, "A", 0), 
-            new Edge(5, "B", 1),
-            new Edge(4, "C", 2)
-        });
-        Node A = new Node("A", new Edge[]{
-            new Edge(2, "O", 0),
-            new Edge(2, "B", 1),
-            new Edge(7, "D", 2),
-            new Edge(12, "F", 3),
-        });
-        Node B = new Node("B", new Edge[]{
-            new Edge(5, "O", 0),
-            new Edge(2, "A", 1),
-            new Edge(4, "D", 2),
-            new Edge(1, "C", 3),
-            new Edge(3, "E", 4),
-        });
-        Node C = new Node("C", new Edge[]{
-            new Edge(4, "O", 0),
-            new Edge(1, "B", 1),
-            new Edge(4, "E", 2),
-        });
-        Node D = new Node("D", new Edge[]{
-            new Edge(4, "B", 0),
-            new Edge(7, "A", 1),
-            new Edge(1, "E", 2),
-            new Edge(5, "T", 3),
-        });
-        Node E = new Node("E", new Edge[]{
-            new Edge(4, "C", 0),
-            new Edge(3, "B", 1),
-            new Edge(1, "D", 2),
-            new Edge(7, "T", 3),
-        });
-        Node F = new Node("F", new Edge[]{
-            new Edge(12, "A", 0),
-            new Edge(3, "T", 1),
-        });
-        Node T = new Node("T", new Edge[]{
-            new Edge(3, "F", 0),
-            new Edge(5, "D", 1),
-            new Edge(7, "E", 2),
-        });
         
-        unselected.add("A");
-        unselected.add("B");
-        unselected.add("C");
-        unselected.add("D");
-        unselected.add("E");
-        unselected.add("F");
-        //unselected.add("O");
-        unselected.add("T");
+   
+        //System.out.println("Unselected:"+unselected);
         
-        listOfNodes.add(A);
-        listOfNodes.add(B);
-        listOfNodes.add(C);
-        listOfNodes.add(D);
-        listOfNodes.add(E);
-        listOfNodes.add(F);
-        listOfNodes.add(O);
-        listOfNodes.add(T);
-        System.out.println("List of nodes:"+listOfNodes);
-        System.out.println("Unselected:"+unselected);
-        //Exploding the first node
-        container = new ArrayList<Path>();
-        for(Edge a: O.getAdjacentEdges()){
+        runDijkstrasAlgorithm(GraphOne.build(),"O");
+       
+    } 
+    
+    public static void runDijkstrasAlgorithm (ArrayList<Node> listOfNodes, String startingNodeString) {
+        Node startingNode = binarySearchAndReturn(startingNodeString, listOfNodes);
+        //building the unselected arraylist
+        ArrayList<String> unselected  = new ArrayList<>();
+        for (Node node:listOfNodes) {
+            unselected.add(node.getName());
+        } 
+         //Exploding the first node
+         ArrayList<Path> container = new ArrayList<>();
+        for(Edge a: startingNode.getAdjacentEdges()){
             container.add(new Path("O", a.getTargetNode(), a.getWeight()));
         }
         System.out.println("Container:"+container);
@@ -99,22 +45,22 @@ public class Main {
                 indexOfChosenPath = i;
             }
         }
-        binarySearchAndDestroy(shortestPath.getLast());
+        binarySearchAndDestroy(shortestPath.getLast(),unselected);
         
         //Selecting that node and then adding new paths into my container
-        Node chosenNode = binarySearchAndReturn(shortestPath.getLast());
+        Node chosenNode = binarySearchAndReturn(shortestPath.getLast(),listOfNodes);
         
         //here's my main while loop
         while (!chosenNode.getName().equals("T")) {
 
             for (int i =1; i < chosenNode.getAdjacentEdges().length;i++) {
-                cloneAndAdd(indexOfChosenPath,chosenNode.getAdjacentEdges()[i]);
+                cloneAndAdd(indexOfChosenPath,chosenNode.getAdjacentEdges()[i],container);
             }
             container.get(indexOfChosenPath).add(chosenNode.getAdjacentEdges()[0]);
             
             
             for (int i=container.size()-1; i>=0;i--) {
-                if (!binarySearchAndConfirm(container.get(i))) {
+                if (!binarySearchAndConfirm(container.get(i),unselected)) {
                     container.remove(i);
                 }
             }
@@ -131,15 +77,16 @@ public class Main {
             System.out.println("Unselected:"+unselected);
             System.out.println("Container:"+container);
             System.out.println("shortestPath:"+shortestPath);
-            binarySearchAndDestroy(shortestPath.getLast());
-            chosenNode = binarySearchAndReturn(shortestPath.getLast());
+            binarySearchAndDestroy(shortestPath.getLast(),unselected);
+            chosenNode = binarySearchAndReturn(shortestPath.getLast(),listOfNodes);
             System.out.println("chosenNode:"+chosenNode);
         }
         
         System.out.println("This is your shortest path!: "+shortestPath);
         
-    } 
-    public static void binarySearchAndDestroy(String toBeDestroyed){
+    }
+
+    public static void binarySearchAndDestroy(String toBeDestroyed,ArrayList<String> unselected){
        //returns index of the toBeDestroyed within the array
        int min = 0;
        int max = unselected.size()-1;
@@ -160,7 +107,7 @@ public class Main {
        }
     }
     
-    public static Node binarySearchAndReturn(String findThis){
+    public static Node binarySearchAndReturn(String findThis, ArrayList<Node> listOfNodes){
        //returns index of the toBeDestroyed within the array
        int min = 0;
        int max = listOfNodes.size()-1;
@@ -182,7 +129,7 @@ public class Main {
        }
        return null;
     }
-public static boolean binarySearchAndConfirm(Path checkThis){
+public static boolean binarySearchAndConfirm(Path checkThis, ArrayList<String> unselected){
        //returns index of the toBeDestroyed within the array
        int min = 0;
        int max = unselected.size()-1;
@@ -204,11 +151,12 @@ public static boolean binarySearchAndConfirm(Path checkThis){
        }
        return false;
     }
-public static void cloneAndAdd(int indexOfPathIAmCloning, Edge edgeIAmAdding){
+public static ArrayList<Path> cloneAndAdd(int indexOfPathIAmCloning, Edge edgeIAmAdding, ArrayList<Path> container){
         Path clone = new Path(container.get(indexOfPathIAmCloning));
         clone.add(edgeIAmAdding);
         //System.out.println("clone"+clone);
         container.add(clone);
+        return container;
         
     }
     /*public static boolean binarySearchAndConfirm(Path checkThis){
