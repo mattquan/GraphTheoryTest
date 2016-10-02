9fe65069ad7d14e5d2d33b714637f51960a5feaa
stephtam <snowyrox101@gmail.com>
2016-10-01 21:17:02 -0400

    Add files via upload

--- FloorFour.java
+++ FloorFour.java
@@ -4,7 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ProjectSahara;
+package projectsahara;
 import java.util.*;
 /**
  *
@@ -13,20 +13,14 @@ import java.util.*;
 public class FloorFour implements Graph{
     
     public ArrayList<Node> build() {
-        ArrayList <String> elevator = new ArrayList();
-        elevator.add("fourth floor elevator");
-        ArrayList <String> desk = new ArrayList();
-        desk.add("PICU");
-        desk.add("pediatric");
-        desk.add("childrens center");
-        desk.add("kids");
-        desk.add("pediatric intensive care unit");
+        
+        
         return new ArrayList<>(
             Arrays.asList( 
-                new Node("E8/9-4", elevator, new Edge[]{
-                    new Edge(24.321,"desk",0),}),                   
-                new Node("desk", desk, new Edge[]{
-                    new Edge(24.321,"E8/9-4",0)})       
+                new Node(Keys.nodeNameElevatorFloorFour, new Edge[]{
+                    new Edge(24.321,Keys.nodeNameDeskFloorFour,0),}),                   
+                new Node(Keys.nodeNameDeskFloorFour, new Edge[]{
+                    new Edge(24.321,Keys.nodeNameElevatorFloorFour,0)})       
                 ));            
                
     }
